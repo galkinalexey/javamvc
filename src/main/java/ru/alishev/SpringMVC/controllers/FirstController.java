0c207afa75cebc2e0b7e4e6517c3874e3b718a28
Alex GLKN <glkntech@yandex.ru>
2025-02-28 04:07:29 +0300

    MVC Template

---
+++ src/main/java/ru/alishev/SpringMVC/controllers/FirstController.java
@@ -0,0 +1,29 @@
+package ru.alishev.SpringMVC.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.http.HttpRequest;
+
+@Controller
+public class FirstController  {
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname",required = false) String surname, Model model) {
+
+        // System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname + "!");
+
+        return "first/hello";
+
+
+    }
+
+    @GetMapping("/goodbye")
+    public String goodyePage() {
+        return "first/goodbye";
+    }
+}
